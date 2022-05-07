27827581091316ff857184dbdbf446e86ca465db
Seung Gi Min <sammymin@naver.com>
2022-05-07 19:42:09 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User  # 장고가 제공해주는 USER
 import os
 
 class Post(models.Model):
@@ -10,10 +11,10 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author: 추후 작성 예정
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
