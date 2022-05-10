2447d7bb2397ea46d6bc8b1beb5c3e86589e3271
Seung Gi Min <sammymin@naver.com>
2022-05-10 23:50:48 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ class PostList(ListView):
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['no_categories_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
 class PostDetail(DetailView):
@@ -18,7 +18,7 @@ class PostDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['no_categories_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
 
@@ -52,4 +52,26 @@ def post_detail(request, pk):
            'post': post,
       }
    )
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all,
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+
+        }
+    )
+
 
